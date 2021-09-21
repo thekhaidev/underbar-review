04ba0e705fa5a3493a1686c86b475f74c67727a3
Valerie Po <valeriecpo@gmail.com>
2021-09-21 16:07:29 -0700

    Implemented tests for _.indexOf function

--- spec/part1.js
+++ spec/part1.js
@@ -219,7 +219,7 @@
       it('should find 40 in the list', function() {
         var numbers = [10, 20, 30, 40, 50];
 
-        expect(_.indexOf(FILL_ME_IN, 40)).to.equal(3);
+        expect(_.indexOf(numbers, 40)).to.equal(3);
       });
 
       it('should be able to compute indexOf even when the native function is undefined', function() {
@@ -229,14 +229,14 @@
       });
 
       it('returns -1 when the target cannot be found not in the list', function() {
-        var numbers = FILL_ME_IN;
+        var numbers = [1, 2, 3, 4, 5];
 
         expect(_.indexOf(numbers, 35)).to.equal(-1);
       });
 
       it('returns the first index that the target can be found at when there are multiple matches', function() {
-        var numbers = FILL_ME_IN;
-        expect(FILL_ME_IN).to.equal(1);
+        var numbers = [1, 3, 3, 3, 4, 5, 3];
+        expect(_.indexOf(numbers, 3)).to.equal(1);
       });
     });
 
